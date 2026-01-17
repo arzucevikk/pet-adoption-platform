993758d9ee7931482ab26892b5efca16b7bcdd2c
arzucevikk <hycwmnbns5@privaterelay.appleid.com>
2026-01-17 20:48:22 +0300

    Create notificarion_service.py

---
+++ src/backend/services/notificarion_service.py
@@ -0,0 +1,6 @@
+def notify_shelter(shelter_id, message):
+    # Simulated Notification Service (Email/SMS)
+    print(f"[NotificationService] To Shelter({shelter_id}): {message}")
+
+def notify_user(user_id, message):
+    print(f"[NotificationService] To User({user_id}): {message}")
